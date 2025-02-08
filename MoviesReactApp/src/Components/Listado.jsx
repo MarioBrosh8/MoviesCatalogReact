5ed5beaf65fce4ab08df1b0c920383389fc7bde8
Luis Mario Valdez <89179630+MarioBrosh8@users.noreply.github.com>
2025-02-07 22:33:45 -0600

    Update Listado.jsx

--- MoviesReactApp/src/Components/Listado.jsx
+++ MoviesReactApp/src/Components/Listado.jsx
@@ -1,9 +1,22 @@
 export const Listado = () => {
     const peliculas = [1, 2, 3, 4, 5];
 
-    return (
-        
+    function resolveAfter2Seconds() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('resolved');
+        }, 2000);
+      });
+    }
 
+      async function asyncCall() {
+          console.log('calling');
+          const result = await resolveAfter2Seconds();
+          console.log(result);
+      }
+      
+
+        return (
         peliculas.map((n) => (
             <div key={n} className="pelicula">
                 <img src={`https://via.placeholder.com/200x300?text=Película ${n}`} alt={`Película ${n}`} />
