8d806ad3135ef08f651f57d895260fe0448ad1c0
MarioBrosh <valdezmario6@gmail.com>
2025-02-12 20:14:33 -0600

    Add delete functionality

--- MoviesReactApp/src/Components/Listado.jsx
+++ MoviesReactApp/src/Components/Listado.jsx
@@ -17,15 +17,30 @@ export const Listado = ({ movies, setMovies }) => {
         setMovies(parsedMovies);
 
     };
+
+
+    const deleteMovie = (id) => {
+        let movies = JSON.parse(localStorage.getItem('movies'));
+        let filteredMovies = movies.filter((movie) => movie.id !== parseInt(id));
+        setMovies(filteredMovies);
+        localStorage.setItem('movies', JSON.stringify(filteredMovies));   
+    }
+
     return (
         <>
 
             {movies != null ? movies.map((movie) => {
                 return (<article key={movies.id} className="fila-peliculas">
+
                     <div key={movie.id} className="pelicula ">
                         <h1>{movie.title}</h1>
                         <p>{movie.description}</p>
-                    </div></article>)
+
+                        <button>Reset</button>
+                        <button className="delete" onClick={() => deleteMovie(movie.id)}>Delete</button>
+                    </div>
+
+                    </article>)
             })
                 : <h2>No hay peliculas para mostrar</h2>
             }
